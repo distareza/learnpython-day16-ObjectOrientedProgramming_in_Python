e68837d459d174c3aac97c065de68bdefe931e82
Dista Reza <dista.reza@bertelsmann.de>
2022-04-07 07:44:33 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Introduction to Object-Oriented Programming in Python
+
+    turtle is build-in python class https://docs.python.org/3/library/turtle.html
+
+"""
+
+from turtle import Turtle, Screen
+
+raphael = Turtle()
+
+print(raphael)
+
+myscreen = Screen()
+print(myscreen.canvheight)
+
+# this method
+myscreen.exitonclick()
