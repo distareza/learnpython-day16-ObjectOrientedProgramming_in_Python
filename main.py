7f019afa2996aaa0e2a3c1c5f579daeb4c69e24c
Dista Reza <dista.reza@bertelsmann.de>
2022-04-07 08:00:54 +0800

    update

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 """
 Introduction to Object-Oriented Programming in Python
 
-    turtle is build-in python class https://docs.python.org/3/library/turtle.html
+    turtle is build-in graphic python module https://docs.python.org/3/library/turtle.html
+        Turtle : build-in python class
+        Screen : is a class, represent a canvas or windows object to draw turtle
 
+    https://docs.python.org/3/library/turtle.html#turtle.shape
+        available shape : “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+    https://docs.python.org/3/library/turtle.html#turtle.color
 """
 
 from turtle import Turtle, Screen
 
 raphael = Turtle()
 
+raphael.shape("turtle")
+raphael.color("green")
+raphael.forward(100)
+
 print(raphael)
 
 myscreen = Screen()
-print(myscreen.canvheight)
 
-# this method
+# screen has attribute canvas height : canvheight, canvwidth
+print(str(myscreen.canvheight) + " " + str(myscreen.canvwidth))
+
+# this method will exit until you click on exit on canvas
 myscreen.exitonclick()
+
+
